8b7271379d9d1e69a361c9a902d68e7449cb84b8
Nico Nowak (alias Winzu) <50320269+WinzuDarkpaw@users.noreply.github.com>
2022-02-23 13:01:38 +0100

    Update py.py

--- py.py
+++ py.py
@@ -1,54 +1,158 @@
-from flask import Flask
+# Importiert die nötigen Libraries und Funktionen für das Python-Skript.
+import uuid
+from flask import Flask, request, jsonify, abort
 
-# Methoden sind erstmal nur imaginär.
-# (Keine DB vorhanden, kein Plan von Schlange und DBs, zu faul zu googlen)
-
+# Initialisiert den Flask-Server.
 app = Flask(__name__)
 
-# HOLT DIE TODO LISTE MIT DER ID _LIST_ID_.
+# IDs für die initiale Datenstruktur erzeugen.
+# Todo-Listen haben für bessere Testbarkeit eine feste ID.
+user_id_bob = uuid.uuid4()
+user_id_alice = uuid.uuid4()
+user_id_eve = uuid.uuid4()
+todo_list_1_id = '1318d3d1-d979-47e1-a225-dab1751dbe75'
+todo_list_2_id = '3062dc25-6b80-4315-bb1d-a7c86b014c65'
+todo_list_3_id = '44b02e00-03bc-451d-8d01-0c67ea866fee'
+todo_1_id = uuid.uuid4()
+todo_2_id = uuid.uuid4()
+todo_3_id = uuid.uuid4()
+todo_4_id = uuid.uuid4()
+
+# Datenstruktur für die User.
+user_list = [
+    {'id': user_id_bob, 'name': 'Bob'},
+    {'id': user_id_alice, 'name': 'Alice'},
+    {'id': user_id_eve, 'name': 'Eve'},
+]
+
+# Datenstruktur für Todo-Listen.
+todo_lists = [
+    {'id': todo_list_1_id, 'name': 'Einkaufsliste'},
+    {'id': todo_list_2_id, 'name': 'Arbeit'},
+    {'id': todo_list_3_id, 'name': 'Privat'},
+]
+
+# Datenstruktur für die Einträge einer Todo-Liste, zugeordnet durch 'list' (Todo-Listen ID) und durch 'user' (User ID).
+todos = [
+    {'id': todo_1_id, 'name': 'Milch', 'description': '', 'list': todo_list_1_id, 'user': user_id_bob},
+    {'id': todo_2_id, 'name': 'Arbeitsblätter ausdrucken', 'description': '', 'list': todo_list_2_id, 'user': user_id_alice},
+    {'id': todo_3_id, 'name': 'Kinokarten kaufen', 'description': '', 'list': todo_list_3_id, 'user': user_id_eve},
+    {'id': todo_3_id, 'name': 'Eier', 'description': '', 'list': todo_list_1_id, 'user': user_id_eve},
+]
+
+
 @app.route('/todo-list/<list_id>', methods=['GET'])
 def getTodoList(list_id: int):
-    return "Todolist: " + list_id
+    if not listExists(list_id):
+        abort(404)
+    
+    return jsonify([i for i in todos if i['list'] == list_id]), 200
 
-# LÖSCHT DIE TODO LISTE MIT DER ID _LIST_ID_.
+
 @app.route('/todo-list/<list_id>', methods=['DELETE'])
 def deleteTodoList(list_id: int):
-    return "Yo diggi, Liste " + list_id + " wird gelöscht, allah."
+    if not listExists(list_id):
+        abort(404)
+    
+    list_item = None
 
-# FÜGT EINE NEUE LISTE HINZU, ID WIRD AUTOMATISCH VERGEBEN (DB).
+    for todo_list in todo_lists:
+        if todo_list['id'] == list_id:
+            list_item = todo_list
+    
+    todo_lists.remove(list_item)
+
+    return '', 200
+
+
 @app.route('/todo-list', methods=['POST'])
 def addTodoList():
-    return "Liste 42069 wurde hinzugefügt."
+    new_list = request.get_json(force=True)
+    new_list['id'] = uuid.uuid4()
 
-# FÜGT EINEN EINTRAG EINER TODO LISTE HINZU.
+    todo_lists.append(new_list)
+
+    return jsonify(new_list), 200
+
 @app.route('/todo-list/<list_id>/entry', methods=['POST'])
 def addEntryToTodoList(list_id: int):
-    return "Da ist ein neuer Eintrag in der Todo-Liste " + list_id
+    new_entry = request.get_json(force=True)
+    new_entry['id'] = uuid.uuid4()
+    new_entry['list'] = list_id
 
-# AKTUALISIERT EINEN EINTRAG EINER TODO LISTE.
+    todos.append(new_entry)
+
+    return jsonify(new_entry), 200
+
+
 @app.route('/todo-list/<list_id>/entry/<entry_id>', methods=['PUT'])
 def updateEntryFromTodoList(list_id, entry_id):
-    return "Eintrag " + entry_id + " wird in Todo Liste " + list_id + " aktualisiert"
+    if not listExists(list_id):
+        abort(404)
 
-# LÖSCHT EINEN EINTRAG EINER TODO LISTE.
+    entry = jsonify([i for i in todos if i['id'] == entry_id])
+    new_entry = request.get_json(force=True)
+
+    entry['name'] = new_entry['name']
+    entry['description'] = new_entry['description']
+
+    return entry, 200
+    
+
 @app.route('/todo-list/<list_id>/entry/<entry_id>', methods=['DELETE'])
 def deleteEntryFromTodoList(list_id, entry_id):
-    return "Eintrag " + entry_id + " wird in Todo Liste " + list_id + " gelöscht"
+    if not listExists(list_id):
+        abort(404)
+    
+    entry_item = None
 
-# HOLT EINE LISTE ALLER BENUTZER
+    for todo in todos:
+        if todo['id'] == entry_id:
+            entry_item = todo
+
+    todos.remove(entry_item)
+
+    return '', 200
+
 @app.route('/user', methods=['GET'])
 def getUsers():
-    return "Hier stehen Benutzer digga."
+    return jsonify(user_list), 200
 
-# FÜGT EINEN BENUTZER HINZU
 @app.route('/user', methods=['POST'])
 def addUser():
-    return "Benutzer wurde erstellt"
+    new_user = request.get_json(force=True)
+    new_user['id'] = uuid.uuid4()
 
-# LÖSCHT EINEN BENUTZER
+    user_list.append(new_user)
+
+    return jsonify(new_user), 200
+
 @app.route('/user/<user_id>', methods=['DELETE'])
 def deleteUser(user_id):
-    return "Benutzer" + user_id + "wird gelöscht."
+    user_item = None
+
+    for user in user_list:
+        if user['id'] == user_id:
+            user_item = user
+        
+    if not user_item:
+        abort(404)
+
+    user_list.remove(user_item)
+
+    return '', 200
+
+def listExists(list_id: int):
+    list_item = None
+
+    for todo_list in todo_lists:
+        if todo_list['id'] == list_id:
+            list_item = todo_list
+        
+    if not list_item:
+        return False
+
+    return True
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=1)
